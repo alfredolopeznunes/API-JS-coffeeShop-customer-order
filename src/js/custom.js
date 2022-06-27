89fc3de54adb3e9dbebc33823decfe7177a89afa
Alfredo <alfredolopeznunes@gmail.com>
2022-06-26 22:03:40 -0400

    script eliminados o renombrados

--- src/js/custom.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', (e) => {
-    //console.log(e.currentTarget.scrollY);
-    header.classList[e.currentTarget.scrollY > 100 ? 'add' : 'remove']('scroll-active');
-});
