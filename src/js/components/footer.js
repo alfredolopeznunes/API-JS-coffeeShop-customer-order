69b28048e13f453f9c02c1430df7cdb9be17155b
Alfredo <alfredolopeznunes@gmail.com>
2022-06-26 22:05:11 -0400

    header y footer cargan en el dom en componentes

---
+++ src/js/components/footer.js
@@ -0,0 +1,21 @@
+class Footer extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="mt-3 pt-4 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+            <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <img src="/assets/images/FoodZero.png" width="50" />
+            </a>
+            <h5 class="text-muted">by ALfredo López Nunes</h5>
+            </div>
+        
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+            </ul>
+        </footer>`;
+    }
+}
+
+customElements.define('footer-template', Footer);
---
+++ src/js/components/header.js
@@ -0,0 +1,63 @@
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header class="header p-4">
+            <div class="container-fluid mt-4">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div class="d-flex align-items-end">
+                        <a href="/" class="logo mb-2 mb-lg-0 text-decoration-none">
+                            <img src="/assets/images/FoodZero.png" class="logo-img" alt="FoodZero" title="FoodZero">
+                        </a>
+                        <nav class="nav-menu ms-5">
+                            <div class="hamburger">
+                                <div class="_layer -top"></div>
+                                <div class="_layer -mid"></div>
+                                <div class="_layer -bottom"></div>
+                            </div>
+                            <div class="menuppal">
+                                <ul class="menu-lista">
+                                    <li><a href="#entradas" class="nav-link px-2 text-secondary">Entradas</a></li>
+                                    <li><a href="#fondo" class="nav-link px-2 text-white">Platos de fondo</a></li>
+                                    <li><a href="#sandwichs" class="nav-link px-2 text-secondary">Sandwiches</a></li>
+                                    <li><a href="#ensaladas" class="nav-link px-2 text-secondary">Ensaladas</a></li>
+                                    <li><a href="#agregados" class="nav-link px-2 text-secondary">Agregados</a></li>
+                                    <li><a href="#postres" class="nav-link px-2 text-secondary">Postres</a></li>
+                                    <li><a href="#jugos-bebidas" class="nav-link px-2 text-white">Jugos y bebidas</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <a href="tel:+86852346000" class="btn text-white me-2" tabindex="0" target="_blank">+86 852 346 000</a>
+                        <a href="iniciar-sesion.html" class="btn button-primary">Iniciar sesión</a>
+                    </div>
+                </div>
+            </div>
+        </header>`;
+    }
+}
+
+customElements.define('header-template', Header);
+
+
+//si hay scroll despues de cargar el dom, aplica "toggle" en el header
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', (e) => {
+    //console.log(e.currentTarget.scrollY);
+    header.classList[e.currentTarget.scrollY > 100 ? 'add' : 'remove']('scroll-active');
+});
+
+
+//evento para abrir/cerrar el menu
+let menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu (event) {
+  this.classList.toggle('is-active');
+  document.querySelector( ".menuppal" ).classList.toggle("is_active");
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
