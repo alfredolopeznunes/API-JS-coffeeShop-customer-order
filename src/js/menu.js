afb9d7319a60f891ee888588a5a19c8b6d0057a7
Alfredo <alfredolopeznunes@gmail.com>
2022-07-05 10:22:57 -0400

    update home script

--- src/js/menu.js
+++ src/js/menu.js
@@ -17,9 +17,9 @@ const getCarta = () => {
     })
 };
 
+//Pinta el menu
 const mostrarCarta = (elementos) => {
     
-    //console.log(elementos);
     const menuItems = document.querySelector('.menu-items');
 
     Object.keys(elementos).forEach(key => {
@@ -46,17 +46,12 @@ const mostrarCarta = (elementos) => {
 
     function listadoMenu(articulo) {
 
-        //console.log( Object.values(elementos[articulo]) );
         const items = Object.values(elementos[articulo]);
-        console.log(items);
         
-        items.forEach(item => {
-           
-            //console.log( item );
-            //console.log(articulo);
-                       
+        items.forEach((item, index, arrItems) => {
+
             return document.querySelector(`#${articulo}-listado`).insertAdjacentHTML("beforeend", `
-                <div class="item-lista mt-5 mb-5">
+                <div class="item-lista mt-5 mb-5 ${ index <= 2 ? '' : 'd-none' }">
                     <div class="item-img">
                         <img src="${item.img}" />
                     </div>
@@ -66,53 +61,34 @@ const mostrarCarta = (elementos) => {
                         <span class="item-description">${item.description}</span>
                     </div>
                 </div>
+                ${index === (arrItems.length -1) && index >= 3 ? "<div class='show-more mb-3'><button class='btn button-primary'>Ver más</button></div>" : ''}
             `);
-
         });
-
-        
-        
     }
 
- 
- 
- /*
-    //Mostrar entradas
-    elementos.entradas.forEach(item => {
+    
+    //funcionalidad del boton ver mas
+    const botones = document.querySelectorAll('.show-more .btn')
+    
+    botones.forEach(boton => {
+        boton.addEventListener("click", event => {
+            event.preventDefault();
 
-        let listadoMenu = document.querySelector('#entradas .listado-menu');
-        
-        listadoMenu.insertAdjacentHTML("beforeend", `
-            <div class="item-img">
-                <img src="${item.img}" />
-            </div>
-            <div class="item-info mt-3">
-                <h5 class="item-price">$${item.price}</h5>
-                <h4 class="item-name">${item.name}</h4>
-                <span class="item-description">${item.description}</span>
-            </div>
-        `);
-    });
-
-    //Mostrar fondo
-    elementos.fondo.forEach(item => {
-
-        let listadoMenu = document.querySelector('#fondo .listado-menu');
-        
-        listadoMenu.insertAdjacentHTML("beforeend", `
-            <div class="item-img">
-                <img src="${item.img}" />
-            </div>
-            <div class="item-info mt-3">
-                <h5 class="item-price">$${item.price}</h5>
-                <h4 class="item-name">${item.name}</h4>
-                <span class="item-description">${item.description}</span>
-            </div>
-        `);
-    });
-
-*/
-
+            const elementosHijos = event.currentTarget.parentElement.parentElement.children;
+            const arrHijos = [...elementosHijos];
+            
+            arrHijos.forEach(hijo => {
+                
+                if(hijo.classList.contains('d-none')) {
+                    hijo.classList.remove('d-none');
+                    hijo.classList.add('d-grid');
+                } else if(hijo.classList.contains('d-grid')) {
+                    hijo.classList.remove('d-grid');
+                    hijo.classList.add('d-none');
+                } 
+            })
+        })
+    })
 }
 
 
@@ -120,3 +96,4 @@ const mostrarCarta = (elementos) => {
 document.addEventListener("DOMContentLoaded", () => {
     getCarta();
 });
+
