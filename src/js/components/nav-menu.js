e1eac4a0c4be05192c3150e6b0af98e031c7502b
Alfredo <alfredolopeznunes@gmail.com>
2022-07-06 11:07:39 -0400

    se agrega menu dinamico con items que devuelve el endpoint de menu

---
+++ src/js/components/nav-menu.js
@@ -0,0 +1,30 @@
+import { API } from "../globals.js";
+
+
+//break point
+const breakPoint = 'api/menus';
+
+
+//metodo get de la instancia
+export const getLiMenu = () => {
+    
+    API.get(breakPoint).then(data => {
+        mostrarCarta(data);
+    }).catch(error => {
+        console.log(error);
+    })
+};
+
+//Pinta el nav menu
+const mostrarCarta = (elementos) => {
+    
+    const menuLi = document.querySelector('.menuppal .menu-lista');
+    Object.keys(elementos).forEach(key => {
+        
+        menuLi.insertAdjacentHTML("beforeend", `<li><a href="/#${key}" class="nav-link px-2 text-secondary">${ key == 'fondo' ? 'Platos de ' + key[0].toUpperCase() + key.substring(1).toLowerCase() 
+        : key == 'jugos-bebidas' ? 'Para Beber'
+        : key[0].toUpperCase() + key.substring(1).toLowerCase() 
+       }</a></li>`);
+    })
+
+}
